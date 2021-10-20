fc0bdcd583f0a28b5c0ddc464d39e45919011929
roshana-iprogrammer <roshan@iprogrammer.co>
2021-10-20 19:01:43 +0530

    express first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('./models/User');
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://roshan:roshan1234@cluster0.eeile.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi there');
+}) 
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
+ 
