807a5f436ecc2859dd973ff5003ccec13e541f3a
roshana-iprogrammer <roshan@iprogrammer.co>
2021-10-21 17:37:13 +0530

    completed remaining

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json())
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://roshan:roshan1234@cluster0.eeile.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(mongoUri);
@@ -19,8 +23,8 @@ mongoose.connection.on('error', (err) => {
     console.log('Error connecting to mongo', err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hi there');
+app.get('/', requireAuth,  (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 }) 
 
 app.listen(3000, () => {
